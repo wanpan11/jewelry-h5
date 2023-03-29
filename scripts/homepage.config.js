0d17ac78947b8bb681ee1cd609d56481b234fca2
wanpan <wanpan96@163.com>
2023-03-29 23:37:51 +0800

    init

---
+++ scripts/homepage.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  entry: "./main.tsx",
+  outDir: "./dist",
+  title: "react",
+  publicPath: "https://wanpan11.github.io/my-app/",
+  cssPublicPath: "https://wanpan11.github.io/my-app/",
+};
